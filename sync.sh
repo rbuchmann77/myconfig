cdf1fe8e6576db17aa49ef8af0eafafa263c652f
Richard Buchmann <richard.buchmann@arcure.net>
2021-07-16 10:17:56 +0200

    Disable NERD. Add tabnine. Configure flake8 for linelength.

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}" | xargs readlink -f)"
+_SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}" | xargs readlink -f)"
 
 function err_report {
     >&2 echo "ERROR on line $(caller)"
@@ -8,7 +8,7 @@ function err_report {
 }
 
 function bootstrap {
-    if md5sum -c "${SCRIPT_DIR}/installed.md5" ; then
+    if md5sum -c "${_SCRIPT_DIR}/installed.md5" ; then
         return
     fi
 
@@ -22,20 +22,25 @@ function setup {
 }
 
 function sync {
-    for f in .bashrc \
+    for f in \
              .vim/vimrc \
              .gitconfig \
              .gdbinit.d/init \
              .gdbinit.d/initQt \
              .gdbinit.d/initStl \
+             .config/flake8 \
             ; do
         DIR=$(dirname ~/${f} | xargs readlink -f)
         mkdir -p "${DIR}"
         touch "${HOME}/${f}"
-        if ! diff "${SCRIPT_DIR}/${f}" "${HOME}/${f}" ; then
-            meld "${SCRIPT_DIR}/${f}" "${HOME}/${f}"
+        if ! diff "${_SCRIPT_DIR}/${f}" "${HOME}/${f}" ; then
+            meld "${_SCRIPT_DIR}/${f}" "${HOME}/${f}"
         fi
     done
+
+    if ! grep -q ${_SCRIPT_DIR}/bashrc_myconfig ~/.bashrc ; then
+        echo "source ${_SCRIPT_DIR}/bashrc_myconfig" >> ~/.bashrc
+    fi
 }
 
 function main {
